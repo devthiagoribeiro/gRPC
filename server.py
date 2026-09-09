e69e86246a3363bb6f25cffb1a4b82e008dfbc78
thiago <thiagordlr@gmail.com>
2026-09-09 20:42:54 -0300

    adicionar código do servidor

---
+++ server.py
@@ -0,0 +1,56 @@
+from concurrent import futures
+import grpc
+
+import tasks_pb2
+import tasks_pb2_grpc
+import uuid
+
+tasksMemory = []
+
+class TaskService(tasks_pb2_grpc.TaskServiceServicer):
+
+    def CreateTask(self, request, context):
+
+        newTask = {
+            'uuid' : uuid.uuid4(),
+            'title': request.title,
+            'description': request.description,
+            'status': 0
+        }
+        tasksMemory.append(newTask)
+
+        return tasks_pb2.CreateTaskResponse(**newTask)
+    def ListTasks(self, request, context):
+        for task in tasksMemory:
+            yield tasks_pb2.Task(
+                uuid=task['uuid'],
+                title=task["title"],
+                description=task["description"],
+                status=task["status"]
+            )
+    
+    def UpdateTask(self, request, context):
+        for task in tasksMemory:
+            if task['uuid'] == request.uuid:
+                target = task
+        target['title'] = request.title
+        target['description'] = request.description
+        target['status'] = request.status
+
+    def DeleteTask(self, request, context):
+        for i, task in enumerate(tasksMemory):
+            if task['uuid'] == request.uuid:
+                tasksMemory.pop(i)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    tasks_pb2_grpc.add_TaskServiceServicer_to_server(
+        TaskService(), server
+    )
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
