9b6b45687caea7a593506e0b763b5f37851bc539
thiago <thiagordlr@gmail.com>
2026-09-09 21:53:45 -0300

    update no servidor

--- server.py
+++ server.py
@@ -12,22 +12,23 @@ class TaskService(tasks_pb2_grpc.TaskServiceServicer):
     def CreateTask(self, request, context):
 
         newTask = {
-            'uuid' : str(uuid.uuid4()),
-            'title': request.title,
-            'description': request.description,
-            'status': 0
-        }
+                    "uuid": str(uuid.uuid4()),
+                    "title": request.title,
+                    "description": request.description,
+                    "status": 1
+                }
+                
         tasksMemory.append(newTask)
-
-        return tasks_pb2.CreateTaskResponse(**newTask)
+        return tasks_pb2.CreateTaskResponse(task=tasks_pb2.Task(**newTask))
+    
     def ListTasks(self, request, context):
         for task in tasksMemory:
-            yield tasks_pb2.Task(
+            yield tasks_pb2.ListTasksResponse(task=tasks_pb2.Task(
                 uuid=task['uuid'],
                 title=task["title"],
                 description=task["description"],
                 status=task["status"]
-            )
+            ))
     
     def UpdateTask(self, request, context):
         for task in tasksMemory:
@@ -37,10 +38,13 @@ class TaskService(tasks_pb2_grpc.TaskServiceServicer):
         target['description'] = request.description
         target['status'] = request.status
 
+        return tasks_pb2.UpdateTaskResponse(task=tasks_pb2.Task(**target))
+
     def DeleteTask(self, request, context):
         for i, task in enumerate(tasksMemory):
             if task['uuid'] == request.uuid:
                 tasksMemory.pop(i)
+        return tasks_pb2.DeleteTaskResponse()
 
 
 def serve():
